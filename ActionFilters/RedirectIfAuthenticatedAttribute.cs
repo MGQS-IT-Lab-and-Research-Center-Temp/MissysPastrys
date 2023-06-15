25d08af0ad1a24478fbb82cad68070a9039e0150
faranasj <faranasj@gmail.com>
2023-06-15 13:54:28 +0100

    Action filters folder added

---
+++ ActionFilters/RedirectIfAuthenticatedAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MissysPastrys.ActionFilters
+{
+    public class RedirectIfAuthenticatedAttribute : Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new RedirectToActionResult("Index", "Home", null);
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+        }
+    }
+}
