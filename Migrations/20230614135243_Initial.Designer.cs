a5ad2a3b29857805b49fc60263ac01f869711f47
faranasj <faranasj@gmail.com>
2023-06-17 18:25:53 +0100

    Some partial scipts added to shared folder

--- Migrations/20230614135243_Initial.Designer.cs
+++ Migrations/20230614135243_Initial.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MissysPastrys.Context;
 
@@ -10,9 +11,11 @@ using MissysPastrys.Context;
 namespace MissysPastrys.Migrations
 {
     [DbContext(typeof(MissysPastrysDbContext))]
-    partial class MissysPastrysDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230614135243_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
